9c4dfd570aa9d5678dab5ee67f8943d8c59bc371
Sai Teja G <saiteja.g@esahai.in>
2017-07-15 16:03:50 +0530

    final db script

--- db.sql
+++ db.sql
@@ -16,6 +16,100 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `child_assess`
+--
+
+DROP TABLE IF EXISTS `child_assess`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `child_assess` (
+  `oid` int(11) DEFAULT NULL,
+  `sid` int(11) DEFAULT NULL,
+  `sem` int(11) DEFAULT NULL,
+  `year` year(4) DEFAULT NULL,
+  `assumed_age` int(11) DEFAULT NULL,
+  `lang_diff` int(11) DEFAULT NULL,
+  `q1` int(11) DEFAULT NULL,
+  `q2` int(11) DEFAULT NULL,
+  `q3` int(11) DEFAULT NULL,
+  `q4` int(11) DEFAULT NULL,
+  `q5` int(11) DEFAULT NULL,
+  `q6` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Child-assesment-table';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `child_assess`
+--
+
+LOCK TABLES `child_assess` WRITE;
+/*!40000 ALTER TABLE `child_assess` DISABLE KEYS */;
+/*!40000 ALTER TABLE `child_assess` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `classroom-assessment`
+--
+
+DROP TABLE IF EXISTS `classroom-assessment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `classroom-assessment` (
+  `schoolid` int(11) NOT NULL,
+  `cid` int(11) NOT NULL,
+  `term` int(11) NOT NULL,
+  `q1` int(11) NOT NULL,
+  `q2` int(11) NOT NULL,
+  `q3` int(11) NOT NULL,
+  `q4` int(11) NOT NULL,
+  `q5` int(11) NOT NULL,
+  `q6` int(11) NOT NULL,
+  `q7` int(11) NOT NULL,
+  `q8` int(11) NOT NULL,
+  `q9` int(11) NOT NULL,
+  `q10` int(11) NOT NULL,
+  `q11` int(11) NOT NULL,
+  `q12` int(11) NOT NULL,
+  `q13` int(11) NOT NULL,
+  `q14` int(11) NOT NULL,
+  `q15` int(11) NOT NULL,
+  `q16` int(11) NOT NULL,
+  `q17` int(11) NOT NULL,
+  `q18` int(11) NOT NULL,
+  `q19` int(11) NOT NULL,
+  `q20` int(11) NOT NULL,
+  `q21` int(11) NOT NULL,
+  `q22` int(11) NOT NULL,
+  `q23` int(11) NOT NULL,
+  `q24` int(11) NOT NULL,
+  `q25` int(11) NOT NULL,
+  `q26` int(11) NOT NULL,
+  `q27` int(11) NOT NULL,
+  `q28` int(11) NOT NULL,
+  `q29` int(11) NOT NULL,
+  `q30` int(11) NOT NULL,
+  `q31` int(11) NOT NULL,
+  `q32` int(11) NOT NULL,
+  `q33` int(11) NOT NULL,
+  `q34` int(11) NOT NULL,
+  `q35` int(11) NOT NULL,
+  `q36` int(11) NOT NULL,
+  `q37` int(11) NOT NULL,
+  `q38` int(11) NOT NULL,
+  PRIMARY KEY (`schoolid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `classroom-assessment`
+--
+
+LOCK TABLES `classroom-assessment` WRITE;
+/*!40000 ALTER TABLE `classroom-assessment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `classroom-assessment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `observer`
 --
 
@@ -35,6 +129,30 @@ CREATE TABLE `observer` (
 LOCK TABLES `observer` WRITE;
 /*!40000 ALTER TABLE `observer` DISABLE KEYS */;
 /*!40000 ALTER TABLE `observer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `observer-student`
+--
+
+DROP TABLE IF EXISTS `observer-student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `observer-student` (
+  `oid` int(11) NOT NULL,
+  `sid` int(11) NOT NULL,
+  `year` date NOT NULL,
+  PRIMARY KEY (`oid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `observer-student`
+--
+
+LOCK TABLES `observer-student` WRITE;
+/*!40000 ALTER TABLE `observer-student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `observer-student` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -100,4 +218,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-15 15:41:12
+-- Dump completed on 2017-07-15 16:03:22
