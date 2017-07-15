3f411452f681e1e46a571f8a4527d96255dacb9a
Sai Teja G <saiteja.g@esahai.in>
2017-07-15 23:06:55 +0530

    charts

--- chart.php
+++ chart.php
@@ -14,27 +14,54 @@
     <canvas id="lineChart" height="450" width="800"></canvas>
   </div>
   <?php
-  $l=new mysqli('13.229.51.247','root','root',mydb);
-  $result=mysqli_query($l,"select oid from observer");
-  $row=mysql_fetch_assoc($result);
+  $l=mysqli_connect('13.229.51.247','root','root','mydb',3306);
+  $array=array();
+  $result=mysqli_query($l,"select * from child_assess");
+  $i=0;
+  while ($row = mysqli_fetch_assoc($result)) {
+  $q1=$row['q1'];
+  $q2=$row['q2'];
+  $q3=$row['q3'];
+  $q4=$row['q4'];
+  $q5=$row['q5'];
+  $q6= $row['q6'];
+  $avg=($q1+$q2+$q3+$q4+$q5+$q6)/6;
+  $avg;
+  $array[$i]=$avg;
+  $i++;
+  }
+  $j=0;
+  $count =array(0,0,0,0,0);
+  $j=0;
+  while($j<$i){
+    $temp=$array[$j];
+    echo $temp;
+    $j++;
+  }
+  /*$j=0;
+  while($j<$i){
+    echo $count[$j];
+    $j++;
+  }*/
+
   ?>
   <script language="JavaScript"><!--
-  var z=<?php echo $row['oid'] ?>;
+  var tempArray = <?php echo json_encode($count); ?>;
   function displayLineChart() {
     var data = {
-        labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        labels: [1, 2, 3, 4, 5],
         datasets: [
             {
-                label: "Prime and Fibonacci",
                 fillColor: "rgba(220,220,220,0.2)",
                 strokeColor: "rgba(220,220,220,1)",
                 pointColor: "rgba(220,220,220,1)",
                 pointStrokeColor: "#fff",
                 pointHighlightFill: "#fff",
                 pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+                data: tempArray,
+                //[1, 3, 5, 7, 11, 13, 17, 19, 23, 29]
             },
-            {
+          /*  {
                 label: "My Second dataset",
                 fillColor: "rgba(151,187,205,0.2)",
                 strokeColor: "rgba(151,187,205,1)",
@@ -42,17 +69,16 @@
                 pointStrokeColor: "#fff",
                 pointHighlightFill: "#fff",
                 pointHighlightStroke: "rgba(151,187,205,1)",
-                data: [z, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-            }
+                data: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+            }*/
         ]
+
     };
     var ctx = document.getElementById("lineChart").getContext("2d");
     var options = { };
     var lineChart = new Chart(ctx).Line(data, options);
   }
+  //window.alert(tempArray);
   --></script>
 </body>
 </html>
-LICENSE#
-
-This block appears to have no license. Please contact the author to request a license.
