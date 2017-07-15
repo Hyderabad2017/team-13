e2cdc028064743f9f8d8c56f63b79c1ed4d1646f
charitha <charitha.yrs@gmail.com>
2017-07-16 00:04:16 +0530

    added

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,7 +18,9 @@
     <link rel="stylesheet" href="css/animate.css">
     <title>Learning Curve</title>
     <script src="js/wow.min.js"></script>
-    <script> new WOW().init();  </script>
+              <script>
+              new WOW().init();
+              </script>
     <link rel="stylesheet" href="css/styles.css">
     <!-- Bootstrap Core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -31,71 +34,38 @@
     <link href="css/grayscale.css" rel="stylesheet">
 
 </head>
-<?php
-include 'dbconnection.php';
-include 'dbfunctions.php';?>
-
 
 <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
 
     <!-- Navigation -->
-    <div class="modal fade" id="myModalHorizontal" tabindex="-1" role="dialog"
-     aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <!-- Modal Header -->
-            <div class="modal-header">
-                <button type="button" class="close"
-                   data-dismiss="modal">
-                       <span aria-hidden="true">&times;</span>
-                       <span class="sr-only">Close</span>
-                </button>
-                <h4 class="modal-title" id="myModalLabel">
-                    Modal title
-                </h4>
-            </div>
-
-            <!-- Modal Body -->
-            <div class="modal-body">
-
-                <form class="form-horizontal" method="post" action="dbfunctions.php" role="form">
-                  <div class="form-group">
-                    <label  class="col-sm-2 control-label"
-                              for="inputEmail3">Name</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control"
-                        id="name" name="name" placeholder="Name"/>
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <label class="col-sm-2 control-label"
-                          for="inputPassword3" >Password</label>
-                    <div class="col-sm-10">
-                        <input type="password" class="form-control"
-                            id="password" name="password" placeholder="Password" required/>
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <div class="col-sm-offset-2 col-sm-10">
-                      <input type="submit" class="btn btn-default" value="Go" name="sub"/>
-                    </div>
-                  </div>
-                </form>
-
-
-
-
-
-
-            </div>
-
-            <!-- Modal Footer -->
-            <div class="modal-footer">
-
-            </div>
+    <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog modal-xs">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Sign In</h4>
         </div>
+        <div class="modal-body">
+          <div class="form-group ">
+            <label for="name" class="col-md-2 control-label" >Name</label>
+
+              <input type="text" class="form-control col-md-offset-2" style="width:20em" id="name" name="lastname" placeholder="Username" required>
+
+          </div>
+          <div class="form-group">
+          <label for="pass" >Password</label>
+
+            <input type="password" name="pass" class="form-control col-md-offset-2" placeholder="your password" style="width:20em"  >
+
+        </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" id="sign-in" class="btn btn-default pull-left col-md-offset-5" data-dismiss="modal">Sign-In</button>
+        </div>
+      </div>
     </div>
-</div><nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+  </div>
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
         <div class="container">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
@@ -114,16 +84,16 @@ include 'dbfunctions.php';?>
                         <a href="#page-top"></a>
                     </li>
                     <li>
-                        <a class="page-scroll" data-toggle="modal" data-target="#myModalHorizontal">Sign In</a>
+                        <a class="page-scroll" data-toggle="modal" data-target="#myModal" href="#about">Sign In</a>
                     </li>
                     <li>
-                        <a class="page-scroll" href="#about">About</a>
+                        <a class="page-scroll" href="#download">Sign Up</a>
                     </li>
                     <li>
-                        <a class="page-scroll" href="#upload">Upload</a>
+                        <a class="page-scroll" href="#contact">Upload</a>
                     </li>
                     <li>
-                        <a class="page-scroll" href="#analytics">Analytics</a>
+                        <a class="page-scroll" href="#contact">Analytics</a>
                     </li>
                 </ul>
             </div>
@@ -139,7 +109,7 @@ include 'dbfunctions.php';?>
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2">
                         <h1 class="brand-heading">Learning Curve</h1>
-                        <p class="intro-text">We care about one simple thing â€“ regardless of economic status, every child has the right to realize oneâ€™s own potential as a human being.We wish to see each child emotionally balanced, self-aware, resilient and able to make informed choices in life.</p>
+                        <p class="intro-text">We care about one simple thing – regardless of economic status, every child has the right to realize one’s own potential as a human being.We wish to see each child emotionally balanced, self-aware, resilient and able to make informed choices in life.</p>
                         <a href="#about" class="btn btn-circle page-scroll">
                             <i class="fa fa-angle-double-down animated"></i>
                         </a>
@@ -154,15 +124,17 @@ include 'dbfunctions.php';?>
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2">
                 <h2>OUR STORY</h2>
-                <p>When Learning Curve began its journey in 2011, we knew well what we wanted to do, but we didnâ€™t know how. Thatâ€™s when it began â€“ the journey of self-reflection, research, numerous interactions with educationists, psychologists and other experts. It took us nearly two years to arrive at what we believed was a comprehensive life skills development program that would enable children to be psychologically, emotionally and socially healthy. Whenever we came across children who were marginalized in any form, our story started taking shape. It was in our third year that we were able to realize the power of life skills â€“ when we walked into a childrenâ€™s home of 40 girls for a visit. As we started working with these girls every weekend for over a year, we realized that more than academics, these children needed help with their overall self-esteem, identity, expression, self-confidence and the opportunity to realize their own potential. As we continued our work with several communities, organizations and children, the concept of emotional and social development resonated with us very strongly. It is the process through which children and acquire and effectively apply the knowledge, attitudes, and skills necessary to understand and manage emotions, set and achieve positive goals, feel and show empathy for others, establish and maintain positive relationships, and make responsible decisions. And thatâ€™s where our story began again â€“ in the form of Learning Curve Life Skills Foundation.</p>
+                <p>When Learning Curve began its journey in 2011, we knew well what we wanted to do, but we didn’t know how. That’s when it began – the journey of self-reflection, research, numerous interactions with educationists, psychologists and other experts. It took us nearly two years to arrive at what we believed was a comprehensive life skills development program that would enable children to be psychologically, emotionally and socially healthy. Whenever we came across children who were marginalized in any form, our story started taking shape. It was in our third year that we were able to realize the power of life skills – when we walked into a children’s home of 40 girls for a visit. As we started working with these girls every weekend for over a year, we realized that more than academics, these children needed help with their overall self-esteem, identity, expression, self-confidence and the opportunity to realize their own potential. As we continued our work with several communities, organizations and children, the concept of emotional and social development resonated with us very strongly. It is the process through which children and acquire and effectively apply the knowledge, attitudes, and skills necessary to understand and manage emotions, set and achieve positive goals, feel and show empathy for others, establish and maintain positive relationships, and make responsible decisions. And that’s where our story began again – in the form of Learning Curve Life Skills Foundation.</p>
             </div>
         </div>
     </section>
     <div class="row">
 
 
+      <div class="fill-screen fixed-attachment" style="background">
 
-    <section id="upload" class="row">
+      </div></div>
+    <section id="download" class="row">
         <div class="container">
           <div class="container">
 
@@ -190,7 +162,7 @@ include 'dbfunctions.php';?>
       <div class="demographic-form" id="d-form" style="display:none">
 
 
-     <form class="form-horizontal" action="index.html" method="post">
+     <form class="form-horizontal" action="dbinsert.php" method="post">
          <div class="form-group">
            <label for="name" class="control-label col-md-2">Student Name</label>
            <div class="col-md-8">
